b00e2ad808933575554e918f99b0fe8cb5d9b416
Susovan Dev <susovandas.dev@gmail.com>
2025-03-30 21:13:28 +0530

    feat(validation): add createUserSchema for Joi validation

---
+++ src/features/user/user.validation.ts
@@ -0,0 +1,13 @@
+import Joi from 'joi';
+
+export const createUserSchema = Joi.object({
+  username: Joi.string().required().min(3).trim(),
+  email: Joi.string()
+    .required()
+    .email()
+    .pattern(new RegExp('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')),
+  fullName: Joi.string().required().min(3),
+  avatar: Joi.string().required(),
+  coverImage: Joi.string().required(),
+  password: Joi.string().required().min(6),
+});
