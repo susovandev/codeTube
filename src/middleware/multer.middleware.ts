e072f3164ba1cff2ea5b596a8b3202dbafef7988
Susovan Dev <susovandas.dev@gmail.com>
2025-03-31 21:33:18 +0530

    feat(upload): add Multer middleware configuration for file uploads

---
+++ src/middleware/multer.middleware.ts
@@ -0,0 +1,50 @@
+import multer from 'multer';
+import path from 'path';
+import { Request } from 'express';
+import { BadRequestError } from '../utils/custom.error';
+
+// Define Storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/temp');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname),
+    );
+  },
+});
+
+// File filter for images and videos
+const fileFilter = (
+  req: Request,
+  file: Express.Multer.File,
+  cb: multer.FileFilterCallback,
+) => {
+  const allowedMimeTypes = [
+    'image/jpeg',
+    'image/png',
+    'image/gif',
+    'video/mp4',
+  ];
+  if (allowedMimeTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(
+      new BadRequestError(
+        'Invalid file type! Only images and videos are allowed.',
+      ),
+    );
+  }
+};
+
+// Multer instance
+export const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+  fileFilter: fileFilter,
+});
